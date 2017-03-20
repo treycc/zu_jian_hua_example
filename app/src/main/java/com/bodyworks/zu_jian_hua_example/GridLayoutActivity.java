3a250783b7c753c14ed018b89a745d83368b905e
treycc <466104603@qq.com>
2017-03-21 00:25:08 +0800

    gridlayout2

--- app/src/main/java/com/bodyworks/zu_jian_hua_example/GridLayoutActivity.java
+++ app/src/main/java/com/bodyworks/zu_jian_hua_example/GridLayoutActivity.java
@@ -5,14 +5,17 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.Space;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayout;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 /**
  * Created by treycc on 2017/3/20.
@@ -43,49 +46,63 @@ public class GridLayoutActivity extends AppCompatActivity {
         DisplayMetrics dm = new DisplayMetrics();// 创建了一张白纸
         windowManager.getDefaultDisplay().getMetrics(dm);// 给白纸设置宽高
 
-//        获取单个ITEM宽度
-        int widthPixels = dm.widthPixels;
-        int heightPixels = dm.heightPixels;
-        Log.w(TAG, "onCreate: " + widthPixels);
-        Log.w(TAG, "onCreate: " + heightPixels);
-        int gridItemMargin = getResources().getDimensionPixelSize(R.dimen.grid_item_margin);
-        int gridMargin = getResources().getDimensionPixelSize(R.dimen.grid_margin);
-        int totalWideMargin = 5 * 2 * gridItemMargin + gridMargin * 2;
-        int totalHeightMargin = 3 * 2 * gridItemMargin + gridMargin * 2;
-        int itemWidth = (widthPixels - totalWideMargin) / 5;
-        int itemHeight = (heightPixels - totalHeightMargin) / 3;
+        if (false) {
+            //        获取单个ITEM宽度
+            int widthPixels = dm.widthPixels;
+            int heightPixels = dm.heightPixels;
+            Log.w(TAG, "onCreate: " + widthPixels);
+            Log.w(TAG, "onCreate: " + heightPixels);
+            int gridItemMargin = getResources().getDimensionPixelSize(R.dimen.grid_item_margin);
+            int gridMargin = getResources().getDimensionPixelSize(R.dimen.grid_margin);
+            int totalWideMargin = 5 * 2 * gridItemMargin + gridMargin * 2;
+            int totalHeightMargin = 3 * 2 * gridItemMargin + gridMargin * 2;
+            int itemWidth = (widthPixels - totalWideMargin) / 5;
+            int itemHeight = (heightPixels - totalHeightMargin) / 3;
 
-        if(true)
-            return;
 
-        for (int i = 0; i < 2; i++) {
-            FrameLayout framItem = new FrameLayout(this);
-            GridLayout.Spec rowSpec = GridLayout.spec(0, 3);
-            GridLayout.Spec colomnSpec = GridLayout.spec(i);
-            GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(rowSpec, colomnSpec);
-            layoutParams.width = itemWidth;
-            layoutParams.setGravity(Gravity.FILL_VERTICAL);
-            layoutParams.setMargins(gridItemMargin, gridItemMargin, gridItemMargin, gridItemMargin);
-            framItem.setLayoutParams(layoutParams);
-            framItem.setBackgroundResource(R.drawable.shape_item_bg);
-            gridLayout.addView(framItem);
-        }
-
-//        int height = gridLayout.getLayoutParams().height / 3;
-//        Log.w(TAG, "onCreate: " + height);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 2; j < 5; j++) {
-                FrameLayout framItem = new FrameLayout(GridLayoutActivity.this);
-                GridLayout.Spec rowSpec = GridLayout.spec(i);
-                GridLayout.Spec colomnSpec = GridLayout.spec(j, 1.0f);
+            for (int i = 0; i < 2; i++) {
+                FrameLayout framItem = new FrameLayout(this);
+                GridLayout.Spec rowSpec = GridLayout.spec(0, 3);
+                GridLayout.Spec colomnSpec = GridLayout.spec(i);
                 GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(rowSpec, colomnSpec);
                 layoutParams.width = itemWidth;
-                layoutParams.height = itemHeight;
+                layoutParams.setGravity(Gravity.FILL_VERTICAL);
                 layoutParams.setMargins(gridItemMargin, gridItemMargin, gridItemMargin, gridItemMargin);
                 framItem.setLayoutParams(layoutParams);
                 framItem.setBackgroundResource(R.drawable.shape_item_bg);
                 gridLayout.addView(framItem);
             }
+
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 2; j < 5; j++) {
+//                FrameLayout framItem = new FrameLayout(GridLayoutActivity.this);
+//                GridLayout.Spec rowSpec = GridLayout.spec(i);
+//                GridLayout.Spec colomnSpec = GridLayout.spec(j, 1.0f);
+//                GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(rowSpec, colomnSpec);
+//                layoutParams.width = itemWidth;
+//                layoutParams.height = itemHeight;
+//                layoutParams.setMargins(gridItemMargin, gridItemMargin, gridItemMargin, gridItemMargin);
+//                framItem.setLayoutParams(layoutParams);
+//                framItem.setBackgroundResource(R.drawable.shape_item_bg);
+//                gridLayout.addView(framItem);
+//            }
+//        }
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    LinearLayout linearLayout = new LinearLayout(this);
+
+                }
+            }
+        } else {
+            int childCount = gridLayout.getChildCount();
+            FrameLayout childAtPostion = (FrameLayout) gridLayout.getChildAt(childCount - 1);
+            ViewGroup.LayoutParams layoutParams = childAtPostion.getLayoutParams();
+            Space space = new Space(this);
+            space.setLayoutParams(layoutParams);
+            gridLayout.removeView(childAtPostion);
+            gridLayout.addView(space);
+
         }
 
 
